e54ce8823a781dfce2036eb761379feb69d5e1cf
Brian hosinski <bphos@outlook.com>
2018-06-05 21:33:56 -0400

    Create adminStuff.lua

---
+++ adminStuff.lua
@@ -0,0 +1,7 @@
+-- Teleports to player
+game.Workspace.P.HumanoidRootPart.CFrame = game.Workspace.P.HumanoidRootPart.CFrame
+
+-- Tinker with fire
+local fire Instance.new("Fire")
+fire.Parent = game.Workspace.P.Head
+fire.Color = Color3.fromRGB(255,255,255)
