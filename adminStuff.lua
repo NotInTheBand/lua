66f5bfc2300e85f5df8d245a243ac72303e40464
Brian hosinski <bphos@outlook.com>
2018-06-05 21:36:42 -0400

    Update adminStuff.lua

--- adminStuff.lua
+++ adminStuff.lua
@@ -1,7 +1,10 @@
 -- Teleports to player
-game.Workspace.P.HumanoidRootPart.CFrame = game.Workspace.P.HumanoidRootPart.CFrame
+Player1 = game.Workspace.P1.HumanoidRootPart.CFrame
+Player2 = game.Workspace.P2.HumanoidRootPart.CFrame
+game.Workspace.Player1.HumanoidRootPart.CFrame = game.Workspace.Player2.HumanoidRootPart.CFrame
 
 -- Tinker with fire
-local fire Instance.new("Fire")
-fire.Parent = game.Workspace.P.Head
+fireParent = game.Workspace
+local fire = Instance.new("Fire")
+fire.Parent = fireParent
 fire.Color = Color3.fromRGB(255,255,255)
